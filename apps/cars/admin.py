5656f0c32a74aa77791e7cb28e9116300d0449a0
CoderB7 <iutstudent2022@gmail.com>
2024-10-11 17:04:44 +0500

    feat: Car admin

--- apps/cars/admin.py
+++ apps/cars/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from unfold.admin import ModelAdmin
+
+from .models import Car
+
+@admin.register(Car)
+class CarAdmin(ModelAdmin):
+    fieldsets = (
+        ("Car Model", {"fields": ("name", "type", )}),# add brand
+        ("Car Info", {"fields": ("transmission", "year" "mileage", "fuel_type", "rating", "price")}),
+        ("Car Exterior", {"fields": ("color", "image")}),
+        ("Car Interior", {"fields": ("doors", "seats")}),
+    )
+    list_display = ("id", "name", "", "type", "rating", "price") # add brand
+    list_filter = ("type", "fuel_type", "transmission")
+    search_fields = ("name", "year", "") # add brand
+    ordering = ("-id", "rating")
+
